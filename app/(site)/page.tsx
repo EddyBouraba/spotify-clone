0c19129612a50d4004dfd8d77420ce26e08bd5b6
EddyBouraba <142597566+EddyBouraba@users.noreply.github.com>
2023-09-28 12:15:33 +0200

    Layout & supabase setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,29 @@
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold ">
+            Content de te revoir
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem
+              image="/images/liked.png"
+              name="Musiques aimées"
+              href="liked"
+            />
+          </div>
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold"> Nouveau son</h1>
+        </div>
+        <div>Liste des musiques</div>
+      </div>
+    </div>
+  );
+}
